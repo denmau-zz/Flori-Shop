aa9d3aae46e4ff2f22651a7aa1033c87c50c3e55
Restyled.io <commits@restyled.io>
2021-03-25 15:42:49 +0000

    Restyled by prettier

--- js/app.js
+++ js/app.js
@@ -3,17 +3,19 @@ const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
-sign_up_btn.addEventListener(
-    "click", () => { container.classList.add("sign-up-mode"); });
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
 
-sign_in_btn.addEventListener(
-    "click", () => { container.classList.remove("sign-up-mode"); });
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
 
 /** google_map js **/
 function myMap() {
   var mapProp = {
-    center : new google.maps.LatLng(40.712775, -74.005973),
-    zoom : 18,
+    center: new google.maps.LatLng(40.712775, -74.005973),
+    zoom: 18,
   };
   var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
